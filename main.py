edf0e2ba788349c25126ae9f492d15a05d9ff866
tamziy <114886626+tamziy@users.noreply.github.com>
2026-01-23 13:52:21 -0600

    Added calling of gemini flash api

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
     print("Hello from ai-agent!")
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    response_metadata = response.usage_metadata
+
+    if response_metadata is None:
+        raise RuntimeError("No response, API request likely")
+
+    print(f"Prompt tokens: {response_metadata.prompt_token_count}")
+    print(f"Response tokens: {response_metadata.candidates_token_count}")
+    print(response.text)
+
+
 
 
 if __name__ == "__main__":
