8c6ba43dfe6a7d797b4407e214ebf37c8b4f20cd
tamziy <114886626+tamziy@users.noreply.github.com>
2026-01-23 21:05:10 -0600

    Added a verbose optional argument

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def main():
 
     parser = argparse.ArgumentParser(description="Gemini LLM")
     parser.add_argument("user_prompt", type=str, help="Enter a prompt for the agent")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    print(args)
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
@@ -27,9 +28,12 @@ def main():
 
     if response_metadata is None:
         raise RuntimeError("No response, API request likely")
+    
+    if args.verbose is True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response_metadata.prompt_token_count}")
+        print(f"Response tokens: {response_metadata.candidates_token_count}")
 
-    print(f"Prompt tokens: {response_metadata.prompt_token_count}")
-    print(f"Response tokens: {response_metadata.candidates_token_count}")
     print(response.text)
 
 
