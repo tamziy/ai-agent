c30d989f85e505d82b99f0cea6ff8f9c69ea7844
tamziy <114886626+tamziy@users.noreply.github.com>
2026-02-02 13:25:40 -0600

    added the ability for the llm to call functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import SYSTEM_PROMPT
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -37,11 +37,25 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response_metadata.prompt_token_count}")
         print(f"Response tokens: {response_metadata.candidates_token_count}")
-
+    
+    function_results = []
     if response.function_calls:
         for call in response.function_calls:
-            print(f"Calling function: {call.name}({call.args})")
-    
+            function_call_result = call_function(call, args.verbose)
+
+            if not function_call_result.parts:
+                raise Exception("types.Content.parts is empty")
+
+            if function_call_result.parts[0] is None:
+                raise Exception("FunctionResponse object is None")
+            
+            function_results.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            
+
+
     print(response.text)
 
 
